7ec65a8549ea5ef2541fc7dfc1efeef18f46c4a3
Lucas Lernoud <lucaslerdb@gmail.com>
2024-01-17 11:16:02 +0100

    fetch db data from client

--- data.sql
+++ data.sql
@@ -1,47 +1,152 @@
 CREATE TYPE category_type AS ENUM ('A', 'B', 'C');
 
-CREATE TABLE PRODUCT (
-id BIGSERIAL NOT NULL PRIMARY KEY,
-name VARCHAR(100) NOT NULL,
-description VARCHAR(500),
-price NUMERIC(10, 2),
-category category_type,
-image_url VARCHAR(255)
-);
-
-CREATE TABLE CUSTOMER (
+/***** Utilisateurs *****/
+CREATE TABLE utilisateurs (
     id SERIAL PRIMARY KEY,
-    last_name VARCHAR(100) NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    address VARCHAR(255),
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    adresse VARCHAR(255),
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    mot_de_passe VARCHAR(255) NOT NULL,
+    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
--- INSERT INTO PRODUCT (name, description, price, category, image_url)
--- VALUES 
---   ('Produit A', 'Description du Produit A', 19.99, 'A', 'https://exemple.com/produitA.jpg'),
---   ('Produit B', 'Description du Produit B', 29.99, 'B', 'https://exemple.com/produitB.jpg'),
---   ('Produit C', 'Description du Produit C', 39.99, 'C', 'https://exemple.com/produitC.jpg');
+/***** Produits *****/
 
-
-
-
-
-CREATE TABLE Panier (
+CREATE TABLE montres (
     id SERIAL PRIMARY KEY,
-    utilisateur_id INT REFERENCES Utilisateurs(id),
-    produit_id INT REFERENCES Produits(id),
-    quantite INT,
-    prix_unitaire NUMERIC(10, 2),
-    total_produit NUMERIC(10, 2),
-    date_ajout TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    nom VARCHAR(255),
+    description TEXT,
+    prix DECIMAL(10, 2),
+    stock INTEGER,
+    categorie_id INTEGER REFERENCES categories(id),
+    marque_id INTEGER REFERENCES marques(id),
+    image_url VARCHAR(255)
 );
-/* 
-    UNIQUE (utilisateur_id, produit_id) -- Assure l'unicité des combinaisons utilisateur/produit
-*/
 
 
-CREATE USER lucas WITH PASSWORD '5962' SUPERUSER;
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255)
+);
+
+CREATE TABLE marques (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255)
+);
+
+
+/***** Commandes *****/
+
+CREATE TABLE commandes (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    date_commande TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etat_commande VARCHAR(50)
+);
+
+CREATE TABLE details_commande (
+    id SERIAL PRIMARY KEY,
+    commande_id INTEGER REFERENCES commandes(id),
+    montre_id INTEGER REFERENCES montres(id),
+    quantite INTEGER,
+    prix_unitaire DECIMAL(10, 2)
+);
+
+/***** Commandes *****/
+CREATE TABLE commandes (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    date_commande TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    etat_commande VARCHAR(50)
+);
+
+CREATE TABLE details_commande (
+    id SERIAL PRIMARY KEY,
+    commande_id INTEGER REFERENCES commandes(id),
+    montre_id INTEGER REFERENCES montres(id),
+    quantite INTEGER,
+    prix_unitaire DECIMAL(10, 2)
+);
+
+
+
+/***** Panier *****/
+CREATE TABLE paniers (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    montre_id INTEGER REFERENCES montres(id),
+    quantite INTEGER
+);
+
+
+/***** Avis et Évaluations: *****/
+CREATE TABLE avis (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    montre_id INTEGER REFERENCES montres(id),
+    commentaire TEXT,
+    note INTEGER,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+/***** Codes Promotionnels *****/
+CREATE TABLE codes_promo (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50),
+    pourcentage_reduction INTEGER,
+    date_expiration DATE
+);
+
+/***** Newsletter *****/
+CREATE TABLE abonnements_newsletter (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    email VARCHAR(255),
+    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/***** Historique des connexions *****/
+CREATE TABLE historique_connexions (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    date_connexion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    adresse_ip VARCHAR(50)
+);
+
+/***** logs de transactions  *****/
+CREATE TABLE logs_transactions (
+    id SERIAL PRIMARY KEY,
+    utilisateur_id INTEGER REFERENCES utilisateurs(id),
+    montant DECIMAL(10, 2),
+    date_transaction TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+/***** Roles et Autorisations *****/
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE permissions (
+    id SERIAL PRIMARY KEY,
+    role_id INTEGER REFERENCES roles(id),
+    autorisation VARCHAR(255)
+);
+
+
+
+
+
+INSERT INTO marques (nom) VALUES
+    ('Rolex'),
+    ('Cartier'),
+    ('Richard Mille');
+
+    INSERT INTO montres (nom, description, prix, stock, categorie_id, marque_id) VALUES
+    ('Santos', 'Super montre', 7500.00, 10, 1, 3),
+    ('Submariner', 'Cool montre', 40000.00, 15, 1, 1),
+    ('Tourbillon raphael nadal', 'montre chère', 500000.00, 5, 3, 3);
--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const PORT = process.env.PORT ?? 8000
 const express = require("express")
+const cors = require("cors")
 const pool = require("./db")
 
 const app = express()
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("hello")
 })
 
+app.get("/user/:userEmail", async (req, res) => {
+  const { userEmail } = req.params
+  console.log(userEmail)
+  try {
+    const user = await pool.query("SELECT * FROM utilisateurs WHERE email=$1", [
+      userEmail,
+    ])
+    res.json(user.rows)
+    console.log()
+  } catch (error) {
+    console.error(error)
+  }
+})
+
 app.get("/product", async (req, res) => {
   try {
-    products = await pool.query("SELECT * FROM product")
+    products = await pool.query("SELECT * FROM montres")
     res.json(products.rows)
   } catch (error) {
     console.error(error)
